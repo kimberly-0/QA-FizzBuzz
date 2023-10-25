97bfae0e0bf9cf967e4a268845128a19303f42f8
Kimberly D <kimberly.dijkmans@outlook.com>
2023-10-25 12:25:27 +0100

    create first test for normal behaviour

--- src/test/java/org/example/TestFizzBuzz.java
+++ src/test/java/org/example/TestFizzBuzz.java
@@ -2,9 +2,12 @@ package org.example;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestFizzBuzz {
 
@@ -23,6 +26,28 @@ public class TestFizzBuzz {
     public void restoreStreams() {
         System.setOut(originalOut);
         System.setErr(originalErr);
+    }
+
+    @Test
+    public void testPositiveNumber() {
+        FizzBuzz.fizzBuzz(15);
+        String expected = "1" + System.lineSeparator() +
+                "2" + System.lineSeparator() +
+                "Fizz" + System.lineSeparator() +
+                "4" + System.lineSeparator() +
+                "Buzz" + System.lineSeparator() +
+                "Fizz" + System.lineSeparator() +
+                "7" + System.lineSeparator() +
+                "8" + System.lineSeparator() +
+                "Fizz" + System.lineSeparator() +
+                "Buzz" + System.lineSeparator() +
+                "11" + System.lineSeparator() +
+                "Fizz" + System.lineSeparator() +
+                "13" + System.lineSeparator() +
+                "14" + System.lineSeparator() +
+                "FizzBuzz" + System.lineSeparator();
+
+        assertEquals(expected, outContent.toString());
     }
 
 }
